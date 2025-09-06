a6e6f3827e7e5173c5cd59799de82e2ceaccc3e5
Parvez-hasan <parvez7788@gmail.com>
2025-09-06 14:09:13 +0600

    headle error and better ul

--- js/index.js
+++ js/index.js
@@ -19,6 +19,19 @@ const displayLevelWord = (words) => {
 const wordContainer = document.getElementById("word-container")
 wordContainer.innerHTML = ""; 
 
+  if (words.length == 0){
+        wordContainer.innerHTML = `
+        <div class=" text-center col-span-full space-y-4 p-8">
+        <img class="mx-auto" src="./assets/alert-error.png" alt="" srcset="">
+      <p class="font-medium text-gray-600 fond-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয় নি</p>
+      <h2 class="text-3xl font-semibold fond-bangla">নেক্সট Lesson এ যান</h2>
+    </div>
+        
+        `
+        return;
+     };
+
+
 // {    id: 101,
 //      level: 2, 
 //      word: 'Market', 
@@ -32,9 +45,9 @@ words.forEach(word => {
     cart.innerHTML= `
 
     <div class="bg-white rounded-lg shadow-sm text-center py-10 px-5 space-y-4">
-      <h2 class="text-xl font-bold">${word.word}</h2>
+      <h2 class="text-xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি " }</h2>
       <p class="font-medium">Meaning /Pronounciation</p>
-      <div class="font-medium fond-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+      <div class="font-medium fond-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি" }"</div>
       <div class="flex justify-between items-center ">
         <button class="btn bg-sky-50 hover:bg-sky-200"><i class="fa-solid fa-circle-info"></i></button>
         <button class="btn bg-sky-50 hover:bg-sky-200"><i class="fa-solid fa-volume-high"></i></button>
@@ -53,6 +66,7 @@ const displayLesson = (lessons) => {
     // 1. get the container
     const levelContainer = document.getElementById("level-last-container");
     levelContainer.innerHTML = "";
+
 
     // 2. get into every lessons
     for (let lesson of lessons) {
